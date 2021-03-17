4d88625a00b87f9aa0981c5e4a4de4c16205f10d
Lestella <rehemae.v@gmail.com>
2021-03-17 23:29:21 +0200

    task 8

---
+++ test_main_page_sticker.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_login(driver):
+    driver.get("http://localhost/litecart/en/")
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "box-most-popular")))
+    all_items = driver.find_elements_by_xpath("//a[contains(@title,'Duck') and (@class='link')]")
+    print("items", len(all_items))
+    for i in all_items:
+        sticker = i.find_elements_by_xpath(".//div[starts-with(@class,'sticker')]")
+        print("sticker", len(sticker))
+        assert len(sticker) == 1
